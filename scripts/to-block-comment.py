c9b0fe754fd6f7e4ed4a4a5ee8dd2232fbfc6ae1
liulibo <liulibo@ztgame.com>
2025-02-21 14:31:20 +0800

    build-*目录不纳入更改;修改工具脚本

--- scripts/to-block-comment.py
+++ scripts/to-block-comment.py
@@ -4,6 +4,7 @@
 import os
 import chardet
 import sys
+import re
 
 CXX_FILES=[".h",".c",".hh",".cc",".hpp",".cpp",".hxx",".cxx",".ipp",".inl",".inc"]
 
@@ -23,8 +24,14 @@ def gbk_to_utf8(des_file, res_file):
         line =str(data,encoding=res['encoding'])
         file.write(line)
     
-
+def has_chinese(line):
+    result = re.compile(u'[\u4e00-\u9fa5]')
+    if result.search(line):
+        return True
+    return False
+    
 def process_dir(dir):
+    
     for root,dirs,files in os.walk(dir):
         for file in files:
             if not is_cxx_file(file):
@@ -32,15 +39,17 @@ def process_dir(dir):
             
             fullpath = os.path.join(root,file)
             print(f"处理文件:{fullpath}")
+            has_changed = False
             # gbk_to_utf8(fullpath,fullpath)
             wlines = []
             try:
                 with open(fullpath,encoding='utf-8') as f:
                     for line in f.readlines():
-                        if line.find("//") >= 0:
+                        if line.find("//") >= 0 and has_chinese(line):
                             line = line.replace("//","/* ")
                             line = line.rstrip() + "*/\n"
                             wlines.append(line)
+                            has_changed = True
                         else:
                             wlines.append(line)
                             
@@ -49,8 +58,9 @@ def process_dir(dir):
                 wlines=[]
                 continue
             
-            with open(fullpath,'w',encoding='utf-8') as f:
-                f.writelines(wlines)
+            if has_changed:
+                with open(fullpath,'w',encoding='utf-8') as f:
+                    f.writelines(wlines)
                 
 
                         
